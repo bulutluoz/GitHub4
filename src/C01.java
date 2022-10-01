7f432f3b2ba4290dab4ee49b438fcebcfa6965de
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-10-01 09:54:59 +0200

    deneme branch v2

--- src/C01.java
+++ src/C01.java
@@ -3,5 +3,6 @@ public class C01 {
     public static void main(String[] args) {
 
         System.out.println("deneme c01");
+
     }
 }
--- src/C05.java
+++ src/C05.java
@@ -2,5 +2,7 @@ public class C05 {
     public static void main(String[] args) {
         System.out.println("c05");
         System.out.println("deneme c05");
+
+        System.out.println("deneme c05 ikinci degisiklik");
     }
 }
